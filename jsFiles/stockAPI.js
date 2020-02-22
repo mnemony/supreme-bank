a1f968d838993edfc29dc8a8688c83a1fc21dbb6
Maksym <maxymprz@gmail.com>
2020-02-22 10:17:08 +0100

    null handler

--- jsFiles/stockAPI.js
+++ jsFiles/stockAPI.js
@@ -2,48 +2,44 @@ const theSymbol = [];
 const currentPrice = [];
 const changePercent = [];
 const companyName = [];
-const insideTable = document.querySelector('#freshdata');
+const insideTable = document.querySelector("#freshdata");
 
 const fetchData = async () => {
-    const response = await axios.get('https://financialmodelingprep.com/api/v3/stock/gainers');
+  const response = await axios.get(
+    "https://financialmodelingprep.com/api/v3/stock/gainers"
+  );
 
-    const stockData = [...response.data.mostGainerStock];
-    stockData.forEach(consoleLoger)
+  const stockData = [...response.data.mostGainerStock];
+  stockData.forEach(consoleLoger);
 
-
-    for (i = 0; i < theSymbol.length; i++) {
-        if (i % 2 != 0) {
-            insideTable.innerHTML += `
+  for (i = 0; i < theSymbol.length; i++) {
+    if (i % 2 != 0) {
+      insideTable.innerHTML += `
             <tr>
-                <td >${companyName[i]}</td>
+                <td >${companyName[i] ? companyName[i] : theSymbol[i]}</td>
                 <td>${theSymbol[i]}</td>
                 <td>${currentPrice[i]}</td>
                 <td class="increaser">${changePercent[i]}</td>
             </tr>
-            `
-        } else {
-            insideTable.innerHTML += `
+            `;
+    } else {
+      insideTable.innerHTML += `
             <tr class="unround">
-                <td>${companyName[i]}</td>
+                <td>${companyName[i] ? companyName[i] : theSymbol[i]}</td>
                 <td>${theSymbol[i]}</td>
                 <td>${currentPrice[i]}</td>
                 <td class="increaser">${changePercent[i]}</td>
+
             </tr>
-            `
-        }
-
-
-
+            `;
     }
-
-
-
-}
-fetchData()
+  }
+};
+fetchData();
 
 function consoleLoger(item) {
-    theSymbol.push(item.ticker);
-    currentPrice.push(item.price);
-    changePercent.push(item.changesPercentage);
-    companyName.push(item.companyName);
+  theSymbol.push(item.ticker);
+  currentPrice.push(item.price);
+  changePercent.push(item.changesPercentage);
+  companyName.push(item.companyName);
 }
